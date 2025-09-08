0dbed51cb945343d3449718978a1e42f0de782d4
Elias-1317 <eliasemersongomes597@gmail.com>
2025-09-08 17:19:09 -0300

    Try except

--- calculadora.py
+++ calculadora.py
@@ -7,29 +7,42 @@ def subtrair(a, b):
 def multiplicacao(a, b):
     return a * b
 
+def divisao(a, b):
+    return a / b
+    
 print("Calculadora Simples")
 print("-"*30)
 print("""
 1. Soma
 2. Subtração
 3. Multiplicação
+4. Divisão
       """)
 
-escolha = input("Escolha uma opção acima: ").lower
+escolha = int(input("Escolha uma opção acima: "))
 
 a = float(input("Digite o primerio número: "))
 
 b = float(input("\nDigite o segundo número: "))
 
-if escolha == "soma" or "1":
+if escolha == 1:
     resultado = soma(a, b)
     print(f"{a} + {b} = {resultado:.2f}")
 
-elif escolha == "subtrair" or "2":
+elif escolha == 2:
     resultado = subtrair(a, b)
     print(f"{a} - {b} = {resultado:.2f}")
 
-elif escolha == "multiplicação" or "3":
+elif escolha == 3:
     resultado = multiplicacao(a, b)
     print(f"{a} x {b} = {resultado:.2f}")
 
+elif escolha == 4:
+    try:
+        b == 0
+        resultado = divisao(a, b)
+        print(f"{a} / {b} = {resultado:.2f}")
+
+    except ZeroDivisionError:
+        print("Erro: Não existe divisão por 0.")
+        
