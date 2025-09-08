e64da5e290048a001ab29dbb1ffeaf921abccc58
Elias-1317 <eliasemersongomes597@gmail.com>
2025-09-08 16:20:28 -0300

    Etapa 2: Adiciona a função de subtração".

--- calculadora.py
+++ calculadora.py
@@ -1,1 +1,13 @@
-def soma()
+def soma(a, b):
+    return a + b
+
+def subtrair(a, b):
+    return a - b
+
+a = float(input("Digite o primerio número: "))
+
+b = float(input("Digite o segundo número: "))
+
+resultado = subtrair(a, b)
+
+print(f"{resultado}")
