23478833c6e417fbfdb643f9ecdbfb0e6ac2cb53
Elias-1317 <eliasemersongomes597@gmail.com>
2025-09-09 15:41:38 -0300

    Etapa 4: Finaliza o programa com a função de divisão e tratamento de erros

--- calculadora.py
+++ calculadora.py
@@ -1,3 +1,4 @@
+#Criando as funções de cada operação
 def soma(a, b):
     return a + b
 
@@ -8,59 +9,78 @@ def multiplicacao(a, b):
     return a * b
 
 def divisao(a, b):
-    if b!=0:
-        resultado = a / b
-        
-        return resultado
-    else:
-        return "Erro: Não existe divisão por 0."
-   
+    #Iniciando o tratamento de erro
+    try:
+        return a / b
     
+    #Fazendo o tratamento de erro se o segundo número for 0
+    except ZeroDivisionError:
+        return "Erro: Não existe divisão por 0."
 
+
+#Iniciando o loop
 while True:
     try:
-        print("\nCalculadora Simples")
+        #Criação do Menu
+        print("\n")
+        print("Calculadora Simples")
         print("-"*30)
-        print("""
-        1. Soma
-        2. Subtração
-        3. Multiplicação
-        4. Divisão
-        5. Sair
-          """)
-
+        print("""1. Soma
+2. Subtração
+3. Multiplicação
+4. Divisão
+5. Sair""")
+        print("-"*30)
+        
+        #Recebendo a operação do usúario
         escolha = int(input("Escolha uma opção acima: "))
 
+        #Iniciando o tratamento das operações
         if 1 <= escolha <= 5:
-
+            
+            #Saindo do programa caso o usúario escolha 5
             if escolha == 5:
                 print("Saindo do Programa...")
                 break
-                
-            a = float(input("Digite o primerio número: "))
 
-            b = float(input("\nDigite o segundo número: "))
+            #Recebendo os números para fazer a operação escolhida
+            a = int(input("\nDigite o primerio número: "))
+
+            b = int(input("\nDigite o segundo número: "))
         
+            #Fazendo o calculo e exibindo o resultado na tela
             if escolha == 1:
                 resultado = soma(a, b)
-                print(f"{a} / {b} = {resultado:.2f}")
+                print("-"*30)
+                print(f"{a} + {b} = {resultado}")
+                print("-"*30)
 
+            #Fazendo o calculo e exibindo o resultado na tela
             elif escolha == 2:
                 resultado = subtrair(a, b)
-                print(f"{a} / {b} = {resultado:.2f}")
+                print("-"*30)
+                print(f"{a} - {b} = {resultado}")
+                print("-"*30)
 
+            #Fazendo o calculo e exibindo o resultado na tela
             elif escolha == 3:
                 resultado = multiplicacao(a, b)
-                print(f"{a} / {b} = {resultado:.2f}")
+                print("-"*30)
+                print(f"{a} x {b} = {resultado}")
+                print("-"*30)
 
+            #Fazendo o calculo e exibindo o resultado na tela
             elif escolha == 4:
                 resultado = divisao(a, b)
-                print(f"{a} / {b} = {resultado:.2f}")
+                print("-"*30)
+                print(f"{a} / {b} = {resultado}")
+                print("-"*30)
 
-            
+        #Tratando caso a pessoa não escolha uma das opções desejadas
         else:
-            print("Erro! Insira um número referente a uma operação.")
+            print("\nErro! Insira um número referente a uma operação.")
 
+    #Tratamento de erro caso a pessoa não digite um número
     except ValueError:
-        print("Insira apenas números.")
+        print("\nInsira apenas números.")
         
